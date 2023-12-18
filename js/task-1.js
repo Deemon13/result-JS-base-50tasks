cb053786885845e2a9baf0f038d354e6379bb646
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2023-12-18 21:15:13 +0300

    added task-1

---
+++ js/task-1.js
@@ -0,0 +1,15 @@
+var a = 5;
+let b = 10;
+const c = 13;
+
+console.log("a: ", a);
+console.log("b: ", b);
+console.log("c: ", c);
+
+a = 10;
+b = 15;
+// c = 18;
+
+console.log("a: ", a);
+console.log("b: ", b);
+console.log("c: ", c);
