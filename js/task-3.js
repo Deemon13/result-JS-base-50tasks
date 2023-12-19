bbb919c6d993ca4aa5168b863c915ed26da140c4
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2023-12-19 12:32:43 +0300

    added task-3

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+let productName = "Инкубатор";
+let productPrice = 1500;
+let productQuantity = 5;
+let resultCost = productQuantity * productPrice;
+
+console.log(
+  `Вы купили ${productName} (${productQuantity} шт.) по ${productPrice} ₽. Всего было потрачено: ${resultCost} ₽`
+);
