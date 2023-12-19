680b11e94481e438858f17c0d56fb7cacfc9492f
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2023-12-19 12:26:38 +0300

    added task-2

---
+++ js/task-2.js
@@ -0,0 +1,12 @@
+// const a = 100;
+// a = 20; // ошибка - изменение значения const
+// console.log(a);
+
+// console.log(a); // ошибка - вывод значения переменной до ее обьявления
+// let a = 15;
+// console.log(a);
+
+console.log(a);
+var a;
+a = 10;
+console.log(a);
