53e789927e4366b7b012afcea06a93a5d768076d
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2023-12-19 13:46:19 +0300

    added task-5

---
+++ js/task-5.js
@@ -0,0 +1,17 @@
+console.log(10 == 10); // true
+console.log(10 != 10); // true --> false
+console.log(12 == "Привет"); // false
+console.log(10 < 11); // true
+console.log(12 > 20); // false
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false || true); // true
+console.log(true || false); // true
+console.log(true || true); // true
+console.log(10 <= 10); // true
+console.log(18 <= 20); // true
+console.log(50 >= 50); // true
+console.log(51 >= 50); // true
+console.log({} === {}); // false
+console.log({} == {}); // false
