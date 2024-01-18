073e5adc0d1ef1ccae9bfc2ce68c1a9e592bf6b1
Deemon13 <dmitriy.fedorenko13@gmail.com>
2024-01-18 14:39:40 +0300

    added theoru-3.html

---
+++ js/theory-3/task-1.js
@@ -0,0 +1,10 @@
+function sumNumbers(numbers) {
+    let total = 0;
+    console.log(arguments);
+}
+
+const result1 = sumNumbers(1, 2, 3, 4, 5);
+console.log(result1); // 15
+
+const result2 = sumNumbers(10, 20, 30);
+console.log(result2); // 60
