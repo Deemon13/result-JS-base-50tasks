9bb832269d047fe7bebcc7d60ccc31c0a43c8e19
Deemon13 <dmitriy.fedorenko13@gmail.com>
2024-01-19 15:02:20 +0300

    some fixes

--- js/theory-4/task-5.js
+++ js/theory-4/task-5.js
@@ -1,19 +1,7 @@
 function isLeapYear(year) {
-  console.log(year);
-  // let startYear = new Date().setFullYear(year, 0, 1);
-  // let endYear = new Date().setFullYear(year, 11, 31);
-  let startYear = new Date(year, 0, 2, 0, 0, 0, 0).getTime();
-  let endYear = new Date(year, 11, 32, 0, 0, 0, 0).getTime();
+  let febDate = new Date(year, 2, 0);
 
-  let testYearBegin = new Date("2023-01-01").getTime();
-  let testYearEnd = new Date("2023-12-31").getTime();
-
-  console.log("startYear:", startYear);
-  console.log("testYearBegin:", testYearBegin);
-  console.log("endYear:", endYear);
-  console.log("testYearEnd:", testYearEnd);
-
-  return (endYear - startYear) / (1000 * 3600 * 24) === 365 ? true : false;
+  return febDate.getDate() === 29 ? true : false;
 }
 
 console.log(isLeapYear(2023)); // false
---
+++ js/theory-4/task-6.js
@@ -0,0 +1,13 @@
+function getLastDayOfMonth(year, month) {
+    let date = new Date(year, month + 1, 0);
+    return date.getDate();
+  }
+
+// июнь 2027
+console.log(getLastDayOfMonth(2027, 5)); // 30
+
+// февраль 2027
+console.log(getLastDayOfMonth(2027, 1)); // 28
+
+// январь 2027
+console.log(getLastDayOfMonth(2027, 0)); // 31
